019f4465fcd193521992185d581d8eb7a5a6f1ae
timothy12maisha <57938798+timothy12maisha@users.noreply.github.com>
2020-01-27 16:11:14 +0300

    added feature-branch and rebased master

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Cipher {
     public static void main(String[] args){
         Scanner captureUserInput = new Scanner(System.in);
-        System.out.println("Welcome to  Ceaser Cipher");
+        System.out.println("Welcome to  Ceaser Cipher!");
         System.out.println("..........................");
         System.out.println("type '1' to encrypt\n");
         System.out.println("type '2' to decrypt\n");
@@ -12,9 +12,9 @@ public class Cipher {
         Encode encoder = new Encode();
         Decode decoder = new Decode();
         if( choiceInput == 1){
-            System.out.println( "Enter the word to encrypt now");
+            System.out.println( "Enter the word to encrypt now!");
             String inputToEncode = captureUserInput.next();
-            System.out.println("Enter key to encrypt");
+            System.out.println("Enter key to encrypt!");
             int inputKey = captureUserInput.nextInt();
             System.out.println("The encrypted word is " + encoder.encrypting(inputToEncode, inputKey));
         }
