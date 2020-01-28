900af97bd28a2e33e8bd873ca8ae49bf7f901703
timothy12maisha <57938798+timothy12maisha@users.noreply.github.com>
2020-01-28 14:45:26 +0300

    Add changes to the front end

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -3,28 +3,21 @@ import java.util.Scanner;
 public class Cipher {
     public static void main(String[] args){
         Scanner captureUserInput = new Scanner(System.in);
-        System.out.println("Welcome to  Ceaser Cipher!!");
-        System.out.println("..........................");
-        System.out.println("type '1' to encrypt\n");
-        System.out.println("type '2' to decrypt\n");
-        int choiceInput = captureUserInput.nextInt();
+        System.out.println("Welcome to  Ceaser Cipher type sentence below ");
 
         Encode encoder = new Encode();
         Decode decoder = new Decode();
-        if(choiceInput == 1){
+
             System.out.println( "Enter the word to encrypt now!!!");
             String inputToEncode = captureUserInput.next();
             System.out.println("Enter key to encrypt!");
             int inputKey = captureUserInput.nextInt();
             System.out.println("The encrypted word is " + encoder.encrypting(inputToEncode, inputKey));
-        }
-        else if (choiceInput == 2){
             System.out.println( "Enter the word to decrypt now");
             String inputToDecode = captureUserInput.next();
             System.out.println("Enter key to decrypt");
-            int inputKey = captureUserInput.nextInt();
-            System.out.println("The decrypted word is " + decoder.decrypting(inputToDecode, inputKey));
-        }
+            int inputKey1 = captureUserInput.nextInt();
+            System.out.println("The decrypted word is " + decoder.decrypting(inputToDecode, inputKey1));
 
 }
 }
